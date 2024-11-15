ad4ee897105c2c9468e358467bfb01029f75be45
njtierney <nicholas.tierney@gmail.com>
2024-11-15 13:47:39 +1100

    first pass at registering a rstudio addin.

---
+++ R/debugonce_at_cursor.R
@@ -0,0 +1,54 @@
+# borrowed from https://github.com/ropensci/targets/blob/546debc2fd372c7fd961aa492bd20a37f9d3fd31/R/rstudio_addin_tar_load.R#L10
+
+#' @title RStudio addin to call [debugonce()] on the symbol at the cursor.
+#' @description For internal use only. Not a user-side function.
+#' @export
+#' @keywords internal
+#' @param context RStudio API context from
+#'   `rstudioapi::getActiveDocumentContext()`.
+rstudio_addin_debugonce <- function(context = NULL) {
+  context <- context %||% rstudioapi::getActiveDocumentContext()
+  target <- rstudio_symbol_at_cursor(context)
+  if (!is.null(target)) {
+    cat("Running debugonce on", target)
+    env <- list(target = as.symbol(target))
+    eval(substitute(debugonce(target, envir = globalenv()), env = env))
+  }
+}
+
+if_any <- function(condition, x, y) {
+  if (any(condition)) {
+    x
+  } else {
+    y
+  }
+}
+
+# borrowed from https://github.com/ropensci/targets/blob/546debc2fd372c7fd961aa492bd20a37f9d3fd31/R/utils_rstudio.R#L4
+rstudio_symbol_at_cursor <- function(context) {
+  if (identical(context$id, "#console")) {
+    return(NULL)
+  }
+  cursor_pos <- context$selection[[1L]]$range$start
+  cursor_line <- cursor_pos[1L]
+  cursor_column <- cursor_pos[2L]
+  r_symbol_pattern <- "[.A-Za-z][.A-Za-z0-9_]+"
+  line_symbols <- gregexpr(
+    text = context$contents[cursor_line],
+    pattern = r_symbol_pattern
+  )
+  match_starts <- line_symbols[[1L]]
+  match_ends <- match_starts + attr(x = line_symbols[[1]], "match.length") - 1L
+  match_index <- which(
+    cursor_column >= match_starts & cursor_column <= match_ends
+  )
+  if_any(
+    identical(length(match_index), 0L),
+    # cli_red_x("Could not find object name at cursor position."),
+    substr(
+      context$contents[cursor_line],
+      start = match_starts[match_index],
+      stop = match_ends[match_index]
+    )
+  )
+}
